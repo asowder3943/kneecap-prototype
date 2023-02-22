857a51bff10c8d46d08f484a16d4f4e4c454f021
asowder3943 <asowder3943@gmail.com>
2023-02-22 15:55:23 -0500

    move youtube components to individual page

---
+++ static/js/youtube.js
@@ -0,0 +1,8 @@
+function viewSubscriptions(){
+    console.log(document.getElementById('subscriptions-management').style.display = 'block')
+    console.log(document.getElementById('feed').style.display = 'none')}
+
+function viewFeed(){
+    console.log(document.getElementById('subscriptions-management').style.display = 'none')
+    console.log(document.getElementById('feed').style.display = 'block')
+}
